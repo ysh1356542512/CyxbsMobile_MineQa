cef6c04b825c203cd70b331f87be2b5c152d9e11
False-Mask <2623036785@qq.com>
2021-08-03 19:14:43 +0800

    [feature] 完成了邮票明细界面的设计

---
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stampcenter/detail/StampDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.mredrock.cyxbs.mine.page.stampcenter.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mredrock.cyxbs.mine.R
+
+class StampDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mine_activity_stamp_detail)
+    }
+}
