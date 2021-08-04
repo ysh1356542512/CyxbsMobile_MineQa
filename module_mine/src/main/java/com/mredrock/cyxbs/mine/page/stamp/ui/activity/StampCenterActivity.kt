f5f62297f50c7c7e700e00245ee04c8204881ea8
此处填你的名字 <1356542512@qq.com>
2021-08-04 10:57:07 +0800

    [feature]邮物详情页布局编写

--- module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ui/activity/StampCenterActivity.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ui/activity/StampCenterActivity.kt
@@ -1,7 +1,6 @@
 package com.mredrock.cyxbs.mine.page.stamp.ui.activity
 
 import android.os.Bundle
-import android.widget.TableLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -13,22 +12,23 @@ import com.mredrock.cyxbs.mine.page.stamp.ui.fragment.CenterShopFragment
 import kotlinx.android.synthetic.main.mine_activity_stamp_center.*
 
 /**
-* @Date : 2021/8/4
-* @Author : ysh
-* @Request : God bless my code
-*/
+ * @Date : 2021/8/4
+ * @Author : ysh
+ * @Request : God bless my code
+ */
 class StampCenterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mine_activity_stamp_center)
         //先使用kotlin拓展库得到控件 之后会改为dataBinding
         //先进行 viewPager2 和 tablayout 的绑定
-        vp_center.orientation =ViewPager2.ORIENTATION_HORIZONTAL
-        val fragments = arrayListOf<Fragment>(CenterShopFragment(),CenterShopFragment())
-        vp_center.adapter = object : FragmentStateAdapter(this){
+        vp_center.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val fragments = arrayListOf<Fragment>(CenterShopFragment(), CenterShopFragment())
+        vp_center.adapter = object : FragmentStateAdapter(this) {
             override fun getItemCount(): Int {
                 return 2
             }
+
             override fun createFragment(position: Int): Fragment {
                 return fragments[position]
             }
@@ -36,14 +36,14 @@ class StampCenterActivity : AppCompatActivity() {
         vp_center.apply {
             offscreenPageLimit = 2
             //此处可动态设置tabItem布局
-            TabLayoutMediator(tl_center,vp_center, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
-                when(position){
-                    0->{
+            TabLayoutMediator(tl_center, vp_center, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                when (position) {
+                    0 -> {
                         tab.text = "邮票小店"
                         //此处setCustomView来设置布局 tab.setCustomView(R.layout.xxx)
                         //在TabLayoutMediator源码的populateTabsFromPagerAdapter方法中可查看逻辑
                     }
-                    else->{
+                    else -> {
                         tab.text = "邮票任务"
                         //邮票任务在此处进行网络申请 若该日未点击过 则设置带有小红点的布局(在之前统一申请两个fragment和该activity的所有数据)
                         //因为每次加载该Activity的时候都要经过此代码 为了减少onTabSelected处网络申请的次数
@@ -52,7 +52,7 @@ class StampCenterActivity : AppCompatActivity() {
                 }
             }).attach()
         }
-        tl_center.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+        tl_center.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabReselected(tab: TabLayout.Tab?) {
 
             }
--- module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ui/fragment/CenterShopFragment.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ui/fragment/CenterShopFragment.kt
@@ -33,7 +33,7 @@ class CenterShopFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_center_shop, container, false)
+        return inflater.inflate(R.layout.mine_fragment_center_shop, container, false)
     }
 
     companion object {
