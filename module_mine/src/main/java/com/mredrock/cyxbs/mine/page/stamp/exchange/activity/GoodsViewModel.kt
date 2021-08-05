c565cd21eba2377e810c92c3703871c32d225184
此处填你的名字 <1356542512@qq.com>
2021-08-05 23:19:12 +0800

    [feature]小店商品item布局编写和更换更清晰图片

--- module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/exchange/activity/GoodsViewModel.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/exchange/activity/GoodsViewModel.kt
@@ -1,5 +1,7 @@
 package com.mredrock.cyxbs.mine.page.stamp.exchange.activity
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
 
 /**
@@ -9,5 +11,35 @@ import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
 * @Request : God bless my code
 */
 class GoodsViewModel : BaseViewModel() {
-    
+
+    //商品类型
+    private val goodsType = MutableLiveData<Int>()
+    //商品标题
+    private val goodsTitle = MutableLiveData<String>()
+    //商品库存
+    private val goodsAmount = MutableLiveData<Int>()
+    //商品描述
+    private val goodsDescription = MutableLiveData<String>()
+    //商品价格
+    private val goodsPrice = MutableLiveData<Int>()
+    //用户余额
+    private val userAmount = MutableLiveData<Int>()
+
+    val _goodsType:LiveData<Int>
+    get() = goodsType
+
+    val _goodsTitle:LiveData<String>
+    get() = goodsTitle
+
+    val _goodsAmount:LiveData<Int>
+    get() = goodsAmount
+
+    val _goodsDescription:LiveData<String>
+    get() = goodsDescription
+
+    val _goodsPrice:LiveData<Int>
+    get() = goodsPrice
+
+    val _userAmount:LiveData<Int>
+    get() = userAmount
 }
