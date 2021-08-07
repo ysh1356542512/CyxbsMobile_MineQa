8a617f3b8afa8ee75d98de2252d8fe9021e62d49
False-Mask <2623036785@qq.com>
2021-08-07 09:35:34 +0800

    [fix] 修改第一次进入邮票任务不更新时间的bug

--- module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ext/Ext.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/ext/Ext.kt
@@ -6,31 +6,42 @@ import com.mredrock.cyxbs.common.utils.extensions.editor
 import com.mredrock.cyxbs.common.utils.extensions.sharedPreferences
 import java.text.SimpleDateFormat
 import java.util.*
+//SharedPreference
+private var dateSp: SharedPreferences? = null
+//SP数据库的名称
+private const val SP_NAME: String = "DATE_SP_NAME"
+//存入Date(时间)的key值
+private const val DATE_KEY_NAME = "DATA_KEY_NAME"
+
 
 /**
  *@author ZhiQiang Tu
  *@time 2021/8/7  8:20
  *@signature 我们不明前路，却已在路上
  */
-class Ext {
-}
 
+//获取是否是第一次进入
 fun Context.isFirstTimeComeIn(): Boolean {
 
     dateSp = sharedPreferences(SP_NAME)
     val date = dateSp?.getString(DATE_KEY_NAME, "NULL") ?: "NULL"
+    //如果当前值没有传入
     return if (date == "NULL") {
         putDate()
         false
     } else {
-        compareDate(getCurrentDate(), date)
-        true
+        //如果是第一次进入
+        if (compareDate(getCurrentDate(), date)){
+            //存入当前时间
+            putDate()
+            true
+        }
+        //如果不是第一次进入
+        else{
+            false
+        }
     }
 }
-
-private var dateSp: SharedPreferences? = null
-private const val SP_NAME: String = "DATE_SP_NAME"
-private const val DATE_KEY_NAME = "DATA_KEY_NAME"
 
 //放入当前的时间信息
 private fun putDate() {
@@ -56,7 +67,7 @@ private fun compareDate(current: String, before: String): Boolean {
     val currentTime = currentTimeList.map {
         it.toInt()
     }
-
+    //如果当前时间的年月日有一项大于sp数据库的时间那么说明当前是最新的进入情况
     for (i in 0 until 2) {
         if (currentTimeList[i] > beforeTimeList[i]) {
             return true
