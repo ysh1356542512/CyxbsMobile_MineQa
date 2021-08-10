c5af5001d7ad3bfa365efcdb5fbfd4a005eca09f
False-Mask <2623036785@qq.com>
2021-08-10 10:21:17 +0800

    [style] 规划格式

---
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/stamp/center/binder/TasksBinder.kt
@@ -0,0 +1,47 @@
+package com.mredrock.cyxbs.mine.page.stamp.center.binder
+
+import com.mredrock.cyxbs.mine.R
+import com.mredrock.cyxbs.mine.databinding.MineLayoutTaskTitleBinding
+import com.mredrock.cyxbs.mine.databinding.MineTaskRecycleItemMultiProgressBinding
+import com.mredrock.cyxbs.mine.databinding.MineTaskRecycleItemOnePregressBinding
+import com.mredrock.cyxbs.mine.page.stamp.center.model.FirstLevelTask
+import com.mredrock.cyxbs.mine.page.stamp.center.model.MoreTask
+import com.mredrock.cyxbs.mine.page.stamp.center.util.adlmrecyclerview.binder.MultiTypeBinder
+
+/**
+ *@author ZhiQiang Tu
+ *@time 2021/8/10  10:16
+ *@signature 我们不明前路，却已在路上
+ */
+class MultiTaskBinder(val moreTask: MoreTask) :
+    MultiTypeBinder<MineTaskRecycleItemMultiProgressBinding>() {
+    override fun layoutId(): Int = R.layout.mine_task_recycle_item_multi_progress
+
+    override fun areContentsTheSame(other: Any): Boolean = false
+
+    override fun onBindViewHolder(binding: MineTaskRecycleItemMultiProgressBinding) {
+        binding?.data = moreTask
+    }
+}
+
+class OneTaskBinder(val firstLevelTask: FirstLevelTask) :
+    MultiTypeBinder<MineTaskRecycleItemOnePregressBinding>() {
+    override fun layoutId(): Int = R.layout.mine_task_recycle_item_one_pregress
+
+    override fun areContentsTheSame(other: Any): Boolean = false
+
+    override fun onBindViewHolder(binding: MineTaskRecycleItemOnePregressBinding) {
+        binding?.data = firstLevelTask
+    }
+}
+
+class TitleBinder(val title: String) : MultiTypeBinder<MineLayoutTaskTitleBinding>() {
+    override fun layoutId(): Int = R.layout.mine_layout_task_title
+
+    override fun areContentsTheSame(other: Any): Boolean =
+        other is TitleBinder && other.title == title
+
+    override fun onBindViewHolder(binding: MineLayoutTaskTitleBinding) {
+        binding?.title = title
+    }
+}
